6b015ae2c4f68f6bcfa149e3a1bb2146aaa8c145
Michael Wu <mw10013@gmail.com>
2018-01-25 18:43:53 -0500

    graphiql

--- src/clj/lumbox1/routes/home.clj
+++ src/clj/lumbox1/routes/home.clj
@@ -7,26 +7,23 @@
             [com.walmartlabs.lacinia :as l]
             [lumbox1.api :as api]))
 
-(defn upsert-user! [{:keys [params]}]
-  (db/upsert-user! params)
-  (response/ok (assoc params :result "upsert-user succeeded")))
-
 (defn graphql
   [req]
   (let [{{:keys [query variables operation-name operationName]} :params} req]
     (def r req)
-    (response/ok (l/execute api/schema query variables nil {:operation-name (or operation-name operationName)}))))
+    #_(def body (slurp (:body req)))
+    (response/ok (l/execute api/schema (or query (-> req :body slurp)) variables nil
+                            {:operation-name (or operation-name operationName)}))))
 
 (defn home-page []
   (layout/render "home.html"))
 
 (defroutes home-routes
-  (GET "/" [] (home-page))
-           (GET "/user/:email" [email] (response/ok (db/user [:user/email email])))
-           (POST "/upsert-user" req (upsert-user! req))
+           (GET "/" [] (home-page))
            (POST "/graphql" req (graphql req))
            (GET "/graphql" req (graphql req))
-  (GET "/docs" []
-       (-> (response/ok (-> "docs/docs.md" io/resource slurp))
-           (response/header "Content-Type" "text/plain; charset=utf-8"))))
+           (GET "/graphiql" [] (layout/render "graphiql.html"))
+           (GET "/docs" []
+             (-> (response/ok (-> "docs/docs.md" io/resource slurp))
+                 (response/header "Content-Type" "text/plain; charset=utf-8"))))
 
